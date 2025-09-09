18ed8442e980297222bd0482f7c4791fd19bbe7b
Tonghao Zhang <tonghao@bamaicloud.com>
2025-09-08 23:15:53 -0400

    core: metric: support the dcb
    Signed-off-by: Tonghao Zhang <tonghao@bamaicloud.com>

--- core/metrics/dcb.go
+++ core/metrics/dcb.go
@@ -15,25 +15,35 @@
 package collector
 
 import (
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"strconv"
+	"syscall"
 	"unsafe"
 
 	"huatuo-bamai/internal/conf"
-	"huatuo-bamai/internal/log"
 	"huatuo-bamai/pkg/metric"
 	"huatuo-bamai/pkg/tracing"
 
-	"github.com/mdlayher/netlink"
+	"github.com/vishvananda/netlink/nl"
 	"golang.org/x/sys/unix"
 )
 
-const (
-	DCB_CMD_IEEE_GET = 21
+type dcbCollector struct{}
 
+func init() {
+	tracing.RegisterEventTracing("netdev_dcb", newDcb)
+}
+
+func newDcb() (*tracing.EventTracingAttr, error) {
+	return &tracing.EventTracingAttr{
+		TracingData: &dcbCollector{},
+		Flag:        tracing.FlagMetric,
+	}, nil
+}
+
+const (
+	DCB_CMD_IEEE_GET       = 21
 	DCB_ATTR_IFNAME        = 1
 	DCB_ATTR_IEEE_PFC      = 2
 	DCB_ATTR_IEEE_PEER_PFC = 5
@@ -43,7 +53,26 @@ const (
 	IEEE_8021QAZ_MAX_TCS = 8
 )
 
-type ieeePfc struct { // struct ieee_pfc
+const (
+	SizeofDcbmsg  = 4
+	SizeofIEEEPfc = 133
+)
+
+type dcbMsg struct {
+	family  uint8
+	cmd     uint8
+	dcb_pad uint16
+}
+
+func (msg *dcbMsg) Len() int {
+	return SizeofDcbmsg
+}
+
+func (msg *dcbMsg) Serialize() []byte {
+	return (*(*[SizeofDcbmsg]byte)(unsafe.Pointer(msg)))[:]
+}
+
+type ieeePfc struct {
 	PFCCap      uint8
 	PFCEn       uint8
 	MBC         uint8
@@ -52,181 +81,77 @@ type ieeePfc struct { // struct ieee_pfc
 	Indications [IEEE_8021QAZ_MAX_TCS]uint64 // count of the received pfc frames
 }
 
-type dcbMsg struct { // struct dcbmsg
-	family uint8 //nolint:unused
-	cmd    uint8 //nolint:unused
-	_pad   uint16
+func DeserializeIEEEPfc(b []byte) *ieeePfc {
+	return (*ieeePfc)(unsafe.Pointer(&b[0:SizeofIEEEPfc][0]))
 }
 
-func (m *dcbMsg) MarshalBinary() ([]byte, error) {
-	buf := new(bytes.Buffer)
-	if err := binary.Write(buf, binary.LittleEndian, m); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-type dcbCollector struct {
-	dcbNlConn *netlink.Conn
-}
-
-func newDcbCollector() *dcbCollector {
-	dcbNlConn, err := netlink.Dial(unix.NETLINK_ROUTE, nil)
-	if err != nil {
-		log.Errorf("dail dcb: %v", err)
-		return nil
-	}
-
-	return &dcbCollector{
-		dcbNlConn: dcbNlConn,
-	}
-}
-
-func (c *dcbCollector) Close() error {
-	return c.dcbNlConn.Close()
-}
-
-func (c *dcbCollector) newGetReq(ifname string) (*netlink.Message, error) {
-	dcbmsg := &dcbMsg{
+func doDcbRequest(ifname string) ([][]byte, error) {
+	req := nl.NewNetlinkRequest(unix.RTM_GETDCB, 0)
+	req.AddData(&dcbMsg{
 		family: uint8(unix.AF_UNSPEC),
 		cmd:    uint8(DCB_CMD_IEEE_GET),
-	}
-	dcbmsgb, err := dcbmsg.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("marshal dcbmsg: %w", err)
-	}
+	})
+	req.AddData(nl.NewRtAttr(DCB_ATTR_IFNAME, nl.ZeroTerminated(ifname)))
 
-	ae := netlink.NewAttributeEncoder()
-	ae.String(DCB_ATTR_IFNAME, ifname)
-	attrs, err := ae.Encode()
-	if err != nil {
-		return nil, fmt.Errorf("encode attrs: %w", err)
-	}
-
-	return &netlink.Message{
-		Header: netlink.Header{
-			Type:     unix.RTM_GETDCB,
-			Flags:    netlink.Request | netlink.Acknowledge,
-			Sequence: 1, // reuse connection must fixd sequence
-		},
-		Data: append(dcbmsgb, attrs...),
-	}, nil
+	return req.Execute(unix.NETLINK_ROUTE, 0)
 }
 
-func (*dcbCollector) parseIeeePfc(b []byte) (*ieeePfc, error) {
-	if len(b) != int(unsafe.Sizeof(ieeePfc{})) {
-		return nil, fmt.Errorf("invalid struct ieee_pfc length %d", int(unsafe.Sizeof(ieeePfc{})))
-	}
-
-	var pfc ieeePfc
-	if err := binary.Read(bytes.NewReader(b), binary.BigEndian, &pfc); err != nil {
-		return nil, err
-	}
-	return &pfc, nil
-}
-
-func (c *dcbCollector) getIeeePfc(ifname string) (*ieeePfc, error) {
-	req, err := c.newGetReq(ifname)
-	if err != nil {
-		return nil, err
-	}
-	msgs, err := c.dcbNlConn.Execute(*req)
-	if err != nil {
-		return nil, fmt.Errorf("execute netlink: %w", err)
-	}
-
-	var ieeepfc *ieeePfc
-	msgSize := int(unsafe.Sizeof(dcbMsg{}))
-	for _, m := range msgs {
-		if len(m.Data) <= msgSize {
-			log.Infof("invalid dcbmsg length: %d", msgSize)
-			continue
-		}
-
-		ad, err := netlink.NewAttributeDecoder(m.Data[msgSize:])
-		if err != nil {
-			return nil, fmt.Errorf("new attrs decoder: %w", err)
-		}
-		for ad.Next() {
-			switch ad.Type() {
-			case DCB_ATTR_IFNAME:
-			case DCB_ATTR_IEEE:
-				ad.Nested(func(nad *netlink.AttributeDecoder) error {
-					for nad.Next() {
-						switch nad.Type() {
-						case DCB_ATTR_IEEE_PFC:
-							ieeepfc, err = c.parseIeeePfc(nad.Bytes())
-							return err
-						case DCB_ATTR_IEEE_PEER_PFC:
-							// TODO: support peer pfc
-						}
-					}
-					return nil
-				})
-			}
-		}
-	}
-
-	if ieeepfc != nil {
-		return ieeepfc, nil
-	}
-	return nil, fmt.Errorf("%v: not found ieee pfc", ifname)
-}
-
-func init() {
-	tracing.RegisterEventTracing("netdev_dcb", newDcb)
-}
-
-func newDcb() (*tracing.EventTracingAttr, error) {
-	return &tracing.EventTracingAttr{
-		TracingData: newDcbCollector(),
-		Flag:        tracing.FlagMetric,
-	}, nil
-}
-
-func (pfcc *dcbCollector) Update() ([]*metric.Data, error) {
-	type tmp struct {
-		ifname string
-		*ieeePfc
-	}
-	var t []*tmp
-	for _, ifname := range conf.Get().Tracing.Netdev.Whitelist {
-		pfc, err := pfcc.getIeeePfc(ifname)
-		if err != nil {
-			var opErr *netlink.OpError
-			if errors.As(err, &opErr) {
-				if errors.Is(opErr.Err, unix.ENODEV) ||
-					// virtual iface, such as bond, lo etc.
-					errors.Is(opErr.Err, unix.EOPNOTSUPP) {
-					log.Debugf("ifname: %v, get ieee pfc: %v", ifname, opErr.Error())
-					continue
+func parseAttributes(attrs []syscall.NetlinkRouteAttr) (*ieeePfc, error) {
+	for _, a := range attrs {
+		switch a.Attr.Type {
+		case DCB_ATTR_IFNAME:
+		case DCB_ATTR_IEEE:
+			subattrs, _ := nl.ParseRouteAttr(a.Value)
+			for _, s := range subattrs {
+				switch s.Attr.Type {
+				case DCB_ATTR_IEEE_PFC:
+					return DeserializeIEEEPfc(s.Value), nil
+				case DCB_ATTR_IEEE_PEER_PFC:
 				}
 			}
-			log.Warnf("ifname: %v, get ieee pfc: %v", ifname, err)
-		} else {
-			t = append(t, &tmp{
-				ifname:  ifname,
-				ieeePfc: pfc,
-			})
-			log.Debugf("ifname: %s, ieee pfc: %+v", ifname, pfc)
 		}
 	}
 
-	pfcMetrics := []*metric.Data{}
-	for _, data := range t {
-		for i, req := range data.Requests {
-			pfcMetrics = append(pfcMetrics,
-				metric.NewGaugeData("pfc_requests_total", float64(req),
+	return nil, fmt.Errorf("no attr")
+}
+
+func (dcb *dcbCollector) Update() ([]*metric.Data, error) {
+	data := []*metric.Data{}
+
+	for _, ifname := range conf.Get().Tracing.Netdev.Whitelist {
+		msgs, err := doDcbRequest(ifname)
+		if err != nil {
+			if errors.Is(err, unix.ENOTSUP) {
+				continue
+			}
+
+			return nil, err
+		}
+
+		for _, m := range msgs {
+			attrs, err := nl.ParseRouteAttr(m[SizeofDcbmsg:])
+			if err != nil {
+				return nil, err
+			}
+
+			pfc, err := parseAttributes(attrs)
+			if err != nil {
+				return nil, err
+			}
+
+			for i, cnt := range pfc.Requests {
+				data = append(data, metric.NewGaugeData("pfc_send_total", float64(cnt),
 					"count of the sent pfc frames",
-					map[string]string{"device": data.ifname, "prio": strconv.Itoa(i)}))
-		}
-		for i, ind := range data.Indications {
-			pfcMetrics = append(pfcMetrics,
-				metric.NewGaugeData("pfc_indications_total", float64(ind),
+					map[string]string{"device": ifname, "prio": strconv.Itoa(i)}))
+			}
+
+			for i, cnt := range pfc.Indications {
+				data = append(data, metric.NewGaugeData("pfc_received_total", float64(cnt),
 					"count of the received pfc frames",
-					map[string]string{"device": data.ifname, "prio": strconv.Itoa(i)}))
+					map[string]string{"device": ifname, "prio": strconv.Itoa(i)}))
+			}
 		}
 	}
 
-	return pfcMetrics, nil
+	return data, nil
 }
