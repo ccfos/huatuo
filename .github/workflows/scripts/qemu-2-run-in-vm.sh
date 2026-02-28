36fa82ab0fa0b4772a090dd5f1c32143ba4b50b0
fanzu8 <tuzengbing@gmail.com>
2026-02-28 10:38:24 +0800

    ci: qemu: add check for command deps
    Signed-off-by: fanzu8 <tuzengbing@gmail.com>

--- .github/workflows/scripts/qemu-2-run-in-vm.sh
+++ .github/workflows/scripts/qemu-2-run-in-vm.sh
@@ -5,6 +5,30 @@ ARCH=${1:-amd64}
 OS_DISTRO=${2:-ubuntu24.04}
 GOLANG_VERSION="1.24.0"
 
+COMMAMND_DEPS=(
+	"go"
+	"make"
+	"clang"
+	"gcc"
+	"git"
+	"jq"
+	"kubectl"
+	"curl"
+	"wget"
+)
+
+function check_command_deps() {
+	local ok=1
+	for cmd in "${COMMAMND_DEPS[@]}"; do
+		if ! command -v "$cmd" &>/dev/null; then
+			echo "⚠️ $cmd not found"
+			ok=0
+		fi
+	done
+
+	[ $ok -eq 1 ] || exit 1
+}
+
 function print_sys_info() {
 	# sys info
 	uname -a
@@ -33,6 +57,7 @@ function print_sys_info() {
 	crictl version || true
 
 	kubectl get pods -A || true
+	crictl images || true
 	systemctl status kubelet || true
 	ps -ef | grep kubelet | grep -v grep || true
 
@@ -56,8 +81,8 @@ function install_golang() {
 function prapre_test_env() {
 	case $OS_DISTRO in
 	ubuntu*)
-		apt update
-		apt install make libbpf-dev clang git gcc -y
+		apt update >/dev/null
+		apt install make libbpf-dev clang git gcc jq -y >/dev/null
 		;;
 	esac
 
@@ -68,6 +93,7 @@ function prapre_test_env() {
 print_sys_info
 install_golang
 prapre_test_env
+check_command_deps
 
 cd /mnt/host && pwd
 ls -alh /mnt/host
