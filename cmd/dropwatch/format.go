2011a4983a71c032bc8b8b6d3652d0b6b1844f36
Tonghao Zhang <tonghao@bamaicloud.com>
2026-06-08 23:12:07 +0800

    refactor(dropwatch): restructure entry point and move types to pkg/types
    Extract DropWatchTracing to pkg/types/dropwatch.go for shared use.
    Reorganize cmd/dropwatch into main.go (BPF loading, event loop, CLI)
    and format.go (writer implementations: text, JSON, socket via toolstream).
    
    Signed-off-by: zhangtonghao <zhangtonghao@didiglobal.com>
    Signed-off-by: Tonghao Zhang <tonghao@bamaicloud.com>

---
+++ cmd/dropwatch/format.go
@@ -0,0 +1,85 @@
+// Copyright 2026 The HuaTuo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+
+	"huatuo-bamai/internal/packet"
+	"huatuo-bamai/internal/toolstream"
+	"huatuo-bamai/pkg/types"
+)
+
+// writer is the single write destination for a dropwatch session.
+type writer interface {
+	Write(ev *types.DropWatchTracing) error
+}
+
+type textWriter struct{ w io.Writer }
+
+func (s *textWriter) Write(ev *types.DropWatchTracing) error {
+	fmt.Fprintf(s.w, "%s %s %s len=%d dev=%s pid=%d[%s] addr=%s\n",
+		ev.ObservedTimestamp, ev.PacketType, formatDetail(ev.PacketInfo),
+		ev.PacketLen, ev.NetdevName, ev.Pid, ev.Comm, ev.PacketSkbAddr)
+
+	if ev.Stack != "" {
+		for i, frame := range strings.Split(strings.TrimRight(ev.Stack, "\n"), "\n") {
+			if frame != "" {
+				fmt.Fprintf(s.w, "\t#%-2d  %s\n", i, frame)
+			}
+		}
+	}
+
+	return nil
+}
+
+type jsonWriter struct{ w io.Writer }
+
+func (s *jsonWriter) Write(ev *types.DropWatchTracing) error {
+	b, _ := json.Marshal(ev)
+	_, _ = fmt.Fprintf(s.w, "%s\n", b)
+	return nil
+}
+
+type socketWriter struct{ client *toolstream.Client }
+
+func (s *socketWriter) Write(ev *types.DropWatchTracing) error {
+	ev.Source = "client"
+	return s.client.Send(ev)
+}
+
+// newWriter returns the appropriate writer based on flags. client may be nil.
+func newWriter(w io.Writer, outputFmt string, client *toolstream.Client) writer {
+	switch {
+	case client != nil:
+		return &socketWriter{client: client}
+	case outputFmt == "json":
+		return &jsonWriter{w: w}
+	default:
+		return &textWriter{w: w}
+	}
+}
+
+type detailable interface{ Detail() string }
+
+func formatDetail(info packet.PacketInfo) string {
+	if d, ok := info.(detailable); ok {
+		return d.Detail()
+	}
+	return "[?]"
+}
---
+++ pkg/types/dropwatch.go
@@ -0,0 +1,40 @@
+// Copyright 2026 The HuaTuo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import "huatuo-bamai/internal/packet"
+
+// DropWatchTracing is the canonical JSON schema for a dropwatch event,
+// shared between the dropwatch tool (producer) and huatuo-bamai (consumer).
+type DropWatchTracing struct {
+	ObservedTimestamp  string            `json:"observed_timestamp"`
+	Type               string            `json:"type"`
+	Reason             string            `json:"reason,omitempty"`
+	DropSource         string            `json:"drop_source"`
+	Comm               string            `json:"comm"`
+	Pid                uint64            `json:"pid"`
+	ContainerID        string            `json:"container_id,omitempty"`
+	NetdevName         string            `json:"netdev_name"`
+	NetdevIfindex      uint32            `json:"netdev_ifindex"`
+	NetdevQueueMapping uint32            `json:"netdev_queue_mapping"`
+	NetdevLinkStatus   []string          `json:"netdev_linkstatus"`
+	PacketSkbAddr      string            `json:"packet_skb_addr,omitempty"`
+	PacketEthProto     string            `json:"packet_eth_proto"`
+	PacketType         packet.PacketType `json:"packet_type"`
+	PacketLen          uint32            `json:"packet_len"`
+	PacketInfo         packet.PacketInfo `json:"packet_info,omitempty"`
+	Source             string            `json:"source,omitempty"`
+	Stack              string            `json:"stack"`
+}
