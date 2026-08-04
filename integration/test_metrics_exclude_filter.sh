37fb67d7b0c6ec7a24ae77346eb18b3531878be7
Yue Wang <1939455790@qq.com>
2026-08-04 11:55:46 +0800

    style: format metrics filter wait

--- integration/test_metrics_exclude_filter.sh
+++ integration/test_metrics_exclude_filter.sh
@@ -28,8 +28,8 @@ integration_huatuo_bamai_start write_exclude_filter_config
 # filter assertions do not race collector initialization.
 collect_exclude_filter_metrics() {
 	huatuo_bamai_collect_metrics || return 1
-	grep -qE 'huatuo_bamai_netdev_.*device="eth0"' "${HUATUO_BAMAI_TEST_TMPDIR}/metrics.txt" &&
-		grep -qE 'huatuo_bamai_netdev_.*device="eth1"' "${HUATUO_BAMAI_TEST_TMPDIR}/metrics.txt"
+	grep -qE 'huatuo_bamai_netdev_.*device="eth0"' "${HUATUO_BAMAI_TEST_TMPDIR}/metrics.txt" \
+		&& grep -qE 'huatuo_bamai_netdev_.*device="eth1"' "${HUATUO_BAMAI_TEST_TMPDIR}/metrics.txt"
 }
 
 wait_until "${WAIT_HUATUO_BAMAI_TIMEOUT}" \
