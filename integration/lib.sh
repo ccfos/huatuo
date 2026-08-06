6120d254d62da3dc5bde39c5ebafa5b0ab0959ad
Tonghao Zhang <tonghao@bamaicloud.com>
2026-08-06 10:19:01 +0800

    fix(integration): disable cgroup by default
    Disable cgroup integration in the default test helper.
    This keeps integration tests runnable in container environments.

--- integration/lib.sh
+++ integration/lib.sh
@@ -391,6 +391,7 @@ integration_huatuo_bamai_start() {
 			"--procfs-prefix" "${HUATUO_BAMAI_TEST_FIXTURES}"
 			"--disable-storage"
 			"--disable-kubelet"
+			"--disable-cgroup"
 			"--log-debug"
 		)
 	fi
